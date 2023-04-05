b1e348caf7d0c8f42636660fe2948e7475a6c8ed
MurtazaKp <99162517+MurtazaKp@users.noreply.github.com>
2023-04-05 21:33:36 +0530

    change banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
+
 const Banner = () => {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 p-10 align-middle'>
         <div  >
-            <h1 className=' mt-10 '>The ecosystem for building digital presence</h1>
-            <button className='bg-primary rounded-full px-7 py-3 mt-8 text-cust-grey-100 '>Start a Project</button>
+            <h1 className=' mt-10 text-h4 lg:text-h1  '>The ecosystem for building digital presence</h1>
+            <button className='bg-primary rounded-full px-7 py-3 mt-8 text-cust-grey-100 text-xl '>Start a Project</button>
         </div>
 
         <div className='flex justify-center align-middle p-5'>
